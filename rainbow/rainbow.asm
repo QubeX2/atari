ce9715ac71147747eb7668f550d10c0005e7d1f8
Mikael Andersson <qubex2@gmail.com>
2023-01-22 16:32:08 +0100

    learning

--- rainbow/rainbow.asm
+++ rainbow/rainbow.asm
@@ -16,6 +16,46 @@ start:
 ; Main
 ;====================================================================
 main:
+    lda #2
+    sta VBLANK      ;turn on VBLANK
+    sta VSYNC       ;turn on VSYNC
+
+    ; VSYNC 3 scanlines
+    REPEAT 3
+        sta WSYNC
+    REPEND
+
+    lda #0
+    sta VSYNC       ;turn of VSYNC
+
+    ; VBLANK 37 scanlines 
+    ldx #37
+loopvb:
+    sta WSYNC       ;hit WSYNC and wait for next scanline
+    dex
+    bne loopvb
+
+    lda #0
+    sta VBLANK
+
+    ; DISPLAY 192 visible scanlines
+    ldx #192
+loopm:
+    stx COLUBK      ;set the bg color
+    sta WSYNC
+    dex
+    bne loopm
+
+    ; OVERSCAN 30 scanlines 
+    lda #2
+    sta VBLANK
+
+    ldx #30
+loopos:
+    sta WSYNC       ;hit WSYNC and wait for next scanline
+    dex
+    bne loopos
+
     jmp main        ; repeat from start
 
 ;====================================================================
